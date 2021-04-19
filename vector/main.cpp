88e8ae515be27ea873f7217a6d342c3e0500e23e
skprpi <60091575+skprpi@users.noreply.github.com>
2021-04-19 10:52:21 +0300

    Update main.cpp

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,107 +1,107 @@
 #include <iostream>
 
 
-class vector{
+class vector {
 public:
-    vector(size_t size){
+    vector(size_t size) {
         size_ = 0;
         capacity = size;
         data = new int[capacity];
     }
-    
 
-    ~vector(){
-        delete []data;
+
+    ~vector() {
+        delete[]data;
     }
 
-    void push(int value){
-        if (this->size_ + 1 > capacity){
+    void push(int value) {
+        if (size_ == capacity) {
             relocation();
         }
         data[size_] = value;
-        size_ += 1;
+        ++size_;
     }
 
     void pop() {
-        if (size_ == 0){
-            return;
+        if (empty()) {
+            throw "Vector is empty. It seems nothing to `pop()`";
         }
         size_ -= 1;
         data[size_] = NULL;
     }
 
-    size_t size(){
+    size_t size() const {
         return size_;
     }
 
-    bool empty(){
+    bool empty() const {
         return size_ == 0;
     }
 
-    int at(size_t index){
-        if (index >= size_){
+    int at(size_t index) {
+        if (index >= size_) {
             throw "Vector index out of range";
         }
         return data[index];
     }
 
-    void show(){
-        std::cout << std::endl;
-        for (int i = 0; i < size_; i++){
-            std::cout << data[i] << " ";
-        }
-        std::cout << std::endl;
-    }
 
 private:
     size_t size_;
-    int* data;
+    int *data;
     size_t capacity;
 
-    void relocation(){
+    void relocation() {
         int *new_data = new int[this->size_ * 2];
         for (int i = 0; i < this->size_; i++) {
             new_data[i] = data[i];
         }
         capacity = size_ * 2;
-//        delete []data;
+        delete[]data;
         data = new_data;
     }
 };
+
+
+void show(vector &v1) {
+    std::cout << std::endl;
+    for (int i = 0; i < v1.size(); i++) {
+        std::cout << v1.at(i) << " ";
+    }
+    std::cout << std::endl;
+}
 
 int main() {
     using namespace std;
 
     vector v1(2);
     v1.push(10);
-    v1.show();
+    show(v1);
     v1.pop();
-    v1.pop();
-    v1.show();
+    show(v1);
     v1.push(20);
     v1.push(30);
     v1.push(40);
     v1.push(50);
-    v1.show();
+    show(v1);
 //    cout << v1.at(10);
     cout << v1.at(3) << endl;
     cout << v1.at(2) << endl;
     cout << v1.at(0) << endl;
 
     v1.pop();
-    v1.show();
+    show(v1);
     cout << v1.empty() << endl;
     v1.pop();
-    v1.show();
+    show(v1);
     cout << v1.empty() << endl;
     v1.pop();
-    v1.show();
+    show(v1);
     cout << v1.empty() << endl;
     v1.pop();
-    v1.show();
+    show(v1);
     cout << v1.empty() << endl;
 
 
     return 0;
 }
-
